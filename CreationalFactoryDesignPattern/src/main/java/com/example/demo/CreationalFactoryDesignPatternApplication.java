f5129c94df3ae4f4ab2acea522ea9f925a760507
Kankshi Sharma <kankshi@monocept.com>
2022-02-02 13:59:37 +0530

    second commit

---
+++ CreationalFactoryDesignPattern/src/main/java/com/example/demo/CreationalFactoryDesignPatternApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalFactoryDesignPatternApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(CreationalFactoryDesignPatternApplication.class, args);
+
+		GetPlanFactory planFactory = new GetPlanFactory();
+
+		System.out.print("Enter the name of plan for which the bill will be generated: ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String planName = br.readLine();
+		System.out.print("Enter the number of units for bill will be calculated: ");
+		int units = Integer.parseInt(br.readLine());
+
+		Plan p = planFactory.getPlan(planName);
+		// call getRate() method and calculateBill()method of DomesticPaln.
+
+		System.out.print("Bill amount for " + planName + " of  " + units + " units is: ");
+		p.getRate();
+		p.calculateBill(units);
+	}
+
+}
---
+++ CreationalFactoryDesignPattern/src/main/java/com/example/demo/InstitutionalPlan.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+class  InstitutionalPlan extends Plan{  
+   //@override  
+    public void getRate(){   
+        rate=5.50;  
+   }  
+}
